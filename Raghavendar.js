2e8186276f5686ae41010315e388230c9d96f755
MRaghavendar <46696415+MRaghavendar@users.noreply.github.com>
2019-02-20 13:17:46 -0600

    Update Raghavendar.js

--- Raghavendar.js
+++ Raghavendar.js
@@ -2,5 +2,5 @@ function calculation(){
     var firstNumber =  Number(document.getElementById("one").value);
     var secondNumber = Number(document.getElementById("two").value);
   
-   document.getElementById("result").innerHTML = Math.max(firstNumber, secondNumber );
+   document.getElementById("result").innerHTML = Math.max(firstNumber, secondNumber);
 }
