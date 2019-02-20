927972008ba272c82be4b7f82c4f052a6f5d59d2
MRaghavendar <46696415+MRaghavendar@users.noreply.github.com>
2019-02-20 12:50:27 -0600

    new

---
+++ Raghavendar.js
@@ -0,0 +1,6 @@
+function calculation(){
+    var firstNumber = number(document.getElementById("one").Value);
+    var secondNumber = number(document.getElementById("two").Value);
+    var  largerNumber = result;
+    document.getElementById("result").innerHTML = Math.max(one, two);
+}
